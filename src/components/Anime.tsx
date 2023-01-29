86eda2540a37bef7fd04564649c9ef8a43df09d5
onlywillian <willianandrade953@gmail.com>
2023-01-28 21:42:00 -0300

    design finished

--- src/components/Anime.tsx
+++ src/components/Anime.tsx
@@ -1,9 +1,27 @@
-const Anime = ({img, title}: any) => {
+const Anime = ({img, title, desc, genres, score, episodes}: any) => {
     return ( 
-        <div className="bg-gray-200 w-5/6 h-64 mt-10 flex">
+        <div className="bg-gray-200 w-5/6 h-64 mt-10 flex relative">
+            {/* Coisas dos cantos */}
+            <div className="absolute bg-black text-white rounded w-20 -right-4 -top-4 text-xl text-center py-2">
+                {episodes}
+            </div>
+            <div className="absolute bg-yellow-500 rounded w-20 -right-4 -bottom-4 text-white text-xl font-bold text-center py-2">
+                {score}
+            </div>
+
+            {/* O conteúdo */}
             <img src={img} alt="Anime Cover" className="w-40"/>
             <div className="ml-8 mt-4">
-                <h3 className="text-2xl font-medium">{title}</h3>
+                <h3 className="text-2xl font-semibold">{title}</h3>
+                <p className="h-1/2 overflow-hidden text-ellipsis">{desc}</p>
+
+                <div className=" w-full h-2/6 flex items-end">
+                    {genres?.map((genre: any) => (
+                        <span key={genre.mal_id} className="bg-black rounded text-white py-2 px-3 mr-3 cursor-pointer">
+                            {genre.name}
+                        </span>
+                    ))}
+                </div>
             </div>
         </div>
      );
--- src/components/AnimeContainer.tsx
+++ src/components/AnimeContainer.tsx
@@ -10,17 +10,23 @@ const AnimeContainer = () => {
             fetch("https://api.jikan.moe/v4/anime")
                 .then(response => response.json())
                 .then(data => setAnimeList(data.data))
-
-            console.log(animeList);
-            
-        }
-        fetchData()
-    }, [])
+            }
+            fetchData()
+        }, [])
+        console.log(animeList);
 
     return ( 
         <div className="w-full flex flex-col items-center min-h-full">
             {animeList?.map((anime: any) => (
-                <Anime key={anime?.mal_id} img={anime.images.jpg.image_url} title={anime.title}/>
+                <Anime 
+                    key={anime?.mal_id} 
+                    img={anime.images.jpg.image_url} 
+                    title={anime.title} 
+                    desc={anime.synopsis}
+                    genres={anime.genres}
+                    episodes={anime.episodes}
+                    score={anime.score}
+                />
             ))}
         </div>
      );
